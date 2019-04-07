3c5f8701462683e8efb623114e622975b6e4cf31
Muhammad Ansharullah (ROG) <ansorkazama@gmail.com>
2019-04-07 13:10:31 +0700

    first commit

---
+++ app/src/main/java/id/go/sumutprov/asn/chatsimimi/model/ChatModel.java
@@ -0,0 +1,27 @@
+package id.go.sumutprov.asn.chatsimimi.model;
+
+public class ChatModel {
+    public String message;
+    public boolean isSend;
+
+    public ChatModel(String message, boolean isSend) {
+        this.message = message;
+        this.isSend = isSend;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isSend() {
+        return isSend;
+    }
+
+    public void setSend(boolean send) {
+        isSend = send;
+    }
+}
